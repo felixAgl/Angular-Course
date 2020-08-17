ccf65f8407685880c2d77ae605d6181136274164
FelixGarciaLopez <fagal142010@gmail.com>
2020-08-17 17:01:20 -0500

    feat: se crean las rutas de los componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsProductComponent } from './details-product/details-product.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '',  component: HomeComponent},
+{path: 'products', component: ProductsComponent},
+{path: 'products/:id', component :DetailsProductComponent},
+{path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsProductComponent } from './details-product/details-product.component';
+import { PrucductListComponent } from './prucduct-list/prucduct-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    HomeComponent,
+    NotFoundComponent,
+    DetailsProductComponent,
+    PrucductListComponent
   ],
   imports: [
     BrowserModule,
