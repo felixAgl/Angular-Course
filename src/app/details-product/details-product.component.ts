cbd9e8c74fe6b509a10341e495ab982eb08d3e65
FelixGarciaLopez <fagal142010@gmail.com>
2020-08-17 16:59:23 -0500

    feat: se crea componente de deatils products

---
+++ src/app/details-product/details-product.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-details-product',
+  templateUrl: './details-product.component.html',
+  styleUrls: ['./details-product.component.scss']
+})
+export class DetailsProductComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
