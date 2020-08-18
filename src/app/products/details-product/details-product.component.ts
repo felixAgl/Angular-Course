95ec6d8c87b64a3b318d051ba9d5e30a5067b50c
FelixGarciaLopez <fagal142010@gmail.com>
2020-08-17 23:21:56 -0500

    refactor: refactorizacion de codigo y se agrega nuevo boton para volver a home

--- src/app/products/details-product/details-product.component.ts
+++ src/app/products/details-product/details-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
 import { DataService } from 'src/app/shared/data.service';
+import { Location } from '@angular/common';
 @Component({
   selector: 'app-details-product',
   templateUrl: './details-product.component.html',
@@ -9,12 +10,19 @@ import { DataService } from 'src/app/shared/data.service';
 export class DetailsProductComponent implements OnInit {
   public product: any = {};
 
-  constructor( private route: ActivatedRoute, private dataSvc: DataService) { }
+  constructor( private route: ActivatedRoute,
+    private dataSvc: DataService,
+    private location: Location
+    ) {}
 
   ngOnInit(): void {
     const productId = this.route.snapshot.paramMap.get('id');
     [this.product] = this.dataSvc.getProductById(productId);
     console.log ('product->',this.product);
+  }
+
+  onGoBack(){
+    this.location.back();
   }
 
 }
--- src/app/products/product.component.ts
+++ src/app/products/product.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector:'app-product',
-  template:`<p>HolaMundo</p>`
+  selector: 'app-product',
+  template: ` <h2>
+      Name:
+      <a [routerLink]="['/product', product.id]">{{ product.name }}</a>
+    </h2>
+    <div>Price:{{ product.price }}</div>`,
 })
 export class ProductComponent {
-
+  @Input() product;
 }
